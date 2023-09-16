ca71c4018bb88f0abe055242506f4db4d8135b87
federico.m <fedemv93@gmail.com>
2023-09-16 16:40:17 -0300

    agregado de cosas y movido de maquetado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core'
 //import { login } from './maquetado/css/login.css'
 
+
+
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   //styleUrls : ['./login.component.scss',]
-  styleUrls: ['general.css',
-  'footer.css',
-  'login.css',
-  'button.css',
-  '....../WORDLCAPP-2023-GRUPO-4/input.css']
+  styleUrls: ['../Maquetado/css/button.css',
+  '../Maquetado/css/footer.css',
+  '../Maquetado/css/login.css',
+  '../Maquetado/css/input.css']
 })
 export class LoginComponent {
 
