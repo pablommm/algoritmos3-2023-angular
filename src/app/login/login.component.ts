2e1d6ad2c5e307611f83d6880e1a2e97e35fed67
fedemv <fedemv93@gmail.com>
2023-09-16 16:03:25 -0300

    agregado cosas

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core'
+//import { login } from './maquetado/css/login.css'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./general.css','./footer.css','login.css','button.css','input.css']
+  //styleUrls : ['./login.component.scss',]
+  styleUrls: ['general.css',
+  'footer.css',
+  'login.css',
+  'button.css',
+  '....../WORDLCAPP-2023-GRUPO-4/input.css']
 })
 export class LoginComponent {
 
