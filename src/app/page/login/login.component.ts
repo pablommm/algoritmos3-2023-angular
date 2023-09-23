cfc6a459773302cf9d6e2a08303cf9d5bdb0a085
pablommm <murgiapablo@gmail.com>
2023-09-23 01:07:48 -0300

    hacemos el binding para login con una logica muy simple

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core'
 //import { login } from './maquetado/css/login.css'
 
-
-
-
-
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -18,6 +13,54 @@ import { Component } from '@angular/core'
 
 ]
 })
+
 export class LoginComponent {
+
+  title = 'Login'
  
+  login = new Login()
+}
+
+export class Login {
+  usuario = ""
+  password = ""
+
+  ingresar(){
+    if(this.validarUsuario() === true){
+      alert("todo salio pillo")
+    } else{
+      alert("esta todo mal")
+    }
+
+  }
+
+  validarUsuario(){
+    if (this.validarEmail() && this.validarPassword()) {
+      
+      return true
+    }else {
+    return false
+  }
+  }
+
+  validarEmail(){
+    if(this.usuario.includes("@")){
+      return true
+    }else {
+      return false
+    }
+  }
+
+  validarPassword(){
+    if(this.password != ""){
+      
+      return true
+    }else{
+      console.log("la contrasena no debe ser vacia")
+      return false
+    }
+  }
+
+  
+  
 }
