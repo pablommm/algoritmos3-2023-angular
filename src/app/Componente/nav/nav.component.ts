611884e3ea64c19c8639f8fb850e3732f9ed22bd
Juani <juanignacioz1995@gmail.com>
2023-09-24 14:51:35 -0300

    Se agrega funcionabilidad a la hora de clickear en los diferentes casos como tambien el cambio de username

--- src/app/Componente/nav/nav.component.ts
+++ src/app/Componente/nav/nav.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core'
 })
 export class NavComponent {
 
+  nav = new Nav()
+}
+
+export class Nav{
+
+inicio(){
+  alert("vamos al inicio")
+}
+ figuritas(){
+ alert("Desplazamiento a Figuritas")
+ }
+ perfil(){
+  alert("Desplazamiento a Perfil")
+ }
+ sobres(){
+  alert("Desplazamiento a sobres")
+ }
+ exit(){
+  alert("Saliendo... go to login")
+ }
 }
--- src/app/page/perfil-usuario/perfil-usuario.component.ts
+++ src/app/page/perfil-usuario/perfil-usuario.component.ts
@@ -12,5 +12,49 @@ import { Component } from '@angular/core'
 ]
 })
 export class PerfilUsuarioComponent {
+ cambioDeUsuario = new CambioUsername()
+perfil= new Perfil()
+}
+
+
+
+export class CambioUsername {
+  editando = false
+  nombreUsuario='pepe'
+  nuevoNombreUsuario=''
+
+  hizoClick (){
+    console.log(this.editando)
+    alert('Has hecho click en el icono de lápiz')
+    this.editando= true
+    console.log(this.editando)
+    console.log(this.nombreUsuario)
+
+  }
+  guardar() {
+    if (this.nombreUsuario.length != 0) {
+      this.nuevoNombreUsuario = this.nombreUsuario
+    } else {
+      alert('error')
+        }
+    console.log(this.nombreUsuario)
+    console.log(this.nuevoNombreUsuario)
+    this.editando = false
+  }
+  
+}
+export class Perfil {
+
+  faltantes(){
+    alert("go to faltantes")
+
+  }
+  repetidas(){
+    alert("go to repetidas")
+
+  }
+  informacion(){
+    alert("go to form")
+  }
 
 }
