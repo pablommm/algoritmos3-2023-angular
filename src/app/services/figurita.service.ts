0c82a58ba4afe99e6879ab9b093b59288c93e9e5
francopolesel <francopolesel99@gmail.com>
2023-09-26 00:22:20 -0300

    Se sigue trabajando en service, falta todavia

--- src/app/services/figurita.service.ts
+++ src/app/services/figurita.service.ts
@@ -1,42 +1,24 @@
+import { Figurita, FiguritaJSON } from './../dominio/figurita';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
-
+import { lastValueFrom } from 'rxjs';
+import { REST_SERVER_URL } from './configuration'
 @Injectable({
   providedIn: 'root'
 })
 export class FiguritaService {
-  /* private figuritas: any[] = [];
-  figuritas$ = new BehaviorSubject<any[]>(this.figuritas); */
-
-  constructor(private http: HttpClient) {
-  }
-
-  loadFiguritas() {
-    return this.http.get<any[]>('../../assets/listaFiguritas.json')/* .subscribe((figuritasJSON: any) => {
-      if (Array.isArray(figuritasJSON.figuritas)) {
-        this.figuritas = figuritasJSON.figuritas.map((figurita: any) => {
-          let valoracionBase = 100;
-          if (figurita.estaOnFire) {
-            valoracionBase *= 1.2;
-          }
-          if (figurita.esPar) {
-            valoracionBase *= 1.1;
-          }
-          if (figurita['Nivel de Impresión'] === "medio" || figurita['Nivel de Impresión'] === "alto") {
-            valoracionBase *= 0.85;
-          }
-          figurita.ValoracionBase = valoracionBase;
-          return figurita; 
-        });
-  
-        this.figuritas$.next(this.figuritas); 
-      }
-    }); */
-  }
   
 
-  /* getFiguritas(): any[] {
-    return this.figuritas;
-  } */
+  constructor(private httpClient: HttpClient) {
+  }
+
+  async todasLasFiguritas() : Promise<Figurita[]> {
+    const figuritas$ = this.httpClient.get<FiguritaJSON[]>('http://localhost:4200' + '/busquedaFiguritas')
+    const figuritasJSON = await lastValueFrom(figuritas$)
+    return figuritasJSON.map((figuritaJSON) => Figurita.fromJson(figuritaJSON))
+  }
+
+ 
+
 }
+
