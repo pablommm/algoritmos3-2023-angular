0d1a2fde9f335b018e47445685acc94e7b9c9e9f
Juani <juanignacioz1995@gmail.com>
2023-09-26 23:13:29 -0300

    se sube filtros component

--- src/app/services/figurita.service.ts
+++ src/app/services/figurita.service.ts
@@ -1,7 +1,7 @@
-import { Figurita, FiguritaJSON } from './../dominio/figurita';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { lastValueFrom } from 'rxjs';
+import { Figurita, FiguritaJSON } from './../dominio/figurita'
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { lastValueFrom } from 'rxjs'
 import { REST_SERVER_URL } from './configuration'
 @Injectable({
   providedIn: 'root'
@@ -14,12 +14,12 @@ export class FiguritaService {
 
   async todasLasFiguritas(): Promise<Figurita[]> {
     try {
-      const figuritas$ = this.httpClient.get<FiguritaJSON[]>('../../assets/listaFiguritas.json');
-      const figuritasJSON = await lastValueFrom(figuritas$);
-      return figuritasJSON.map((figuritaJSON) => Figurita.fromJson(figuritaJSON));
+      const figuritas$ = this.httpClient.get<FiguritaJSON[]>('../../assets/listaFiguritas.json')
+      const figuritasJSON = await lastValueFrom(figuritas$)
+      return figuritasJSON.map((figuritaJSON) => Figurita.fromJson(figuritaJSON))
     } catch (error) {
-      console.error("Error al obtener los datos de figuritas: ", error);
-      throw error; 
+      console.error("Error al obtener los datos de figuritas: ", error)
+      throw error 
     }
   }
   
