4fdcf6cc913bbebae98c2e69d08579d662580952
Juani <juanignacioz1995@gmail.com>
2023-09-29 21:53:09 -0300

    se quita de perfil el owner

--- src/app/Componente/card/card.component.ts
+++ src/app/Componente/card/card.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core'
 import { Figurita } from 'src/app/dominio/figurita'
-import { Router,ActivatedRoute } from '@angular/router'
+import { Router,ActivatedRoute,NavigationEnd } from '@angular/router'
 
 @Component({
   selector: 'app-card',
@@ -9,15 +9,42 @@ import { Router,ActivatedRoute } from '@angular/router'
 })
 
 export class CardComponent {
+  currentUrl!: string
 
   constructor(private router: Router, private route :ActivatedRoute) {}
   
   @Input() card!: Figurita
 
+  OnInit(){
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+         this.currentUrl = event.url
+      }
+    })
+  }
+
 
   selecionarCard(){
     this.router.navigateByUrl('/DetalleFigurita') //en el futuro hay que modificarlo para que te lleve a la figurita que coresponde
-    
-  }
  
+
 }
+ruta(){
+  return this.currentUrl === '/FiguritaRepetida'
+}
+
+  
+
+
+}
+
+
+
+
+
+ 
+
+
+
+
+
