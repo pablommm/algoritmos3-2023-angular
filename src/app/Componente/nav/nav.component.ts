6e27f9386c5fa2910551be37fc957e9234d6ee31
francopolesel <francopolesel99@gmail.com>
2023-10-03 14:07:01 -0300

    Agrego funcionamiento a burger

--- src/app/Componente/nav/nav.component.ts
+++ src/app/Componente/nav/nav.component.ts
@@ -1,39 +1,41 @@
-import { Component } from '@angular/core'
-import { Router,ActivatedRoute } from '@angular/router'
-
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
+  isAsideVisible = true;
 
-  constructor(private router: Router, private route :ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-  ngOnInit() {
-   
+  ngOnInit() {}
+
+  perfil() {
+    this.router.navigateByUrl('/PerfilUsuario');
+  }
+  inicio() {
+    this.router.navigateByUrl('/BusquedaFiguritas');
+  }
+  figuritas() {
+    this.router.navigateByUrl('/BusquedaFiguritas');
   }
 
-  perfil(){
-    this.router.navigateByUrl('/PerfilUsuario')
-   }
-   inicio(){
-    this.router.navigateByUrl('/BusquedaFiguritas')
+  sobres() {
+    this.router.navigateByUrl('/BusquedaSobre');
   }
-   figuritas(){
-    this.router.navigateByUrl('/BusquedaFiguritas')
-   }
-  
-   sobres(){
-    this.router.navigateByUrl('/BusquedaSobre')
-   }
-   exit(){
-    this.router.navigateByUrl('/Login')
-   }
+  exit() {
+    this.router.navigateByUrl('/Login');
+  }
 
-   activeRoute(ruta: string){
-    return ruta === this.route.snapshot.url.join('/')
-   }
+  toggleAside() {
+    this.isAsideVisible = !this.isAsideVisible;
+  }
+
+  activeRoute(ruta: string) {
+    return ruta === this.route.snapshot.url.join('/');
+  }
 }
 
