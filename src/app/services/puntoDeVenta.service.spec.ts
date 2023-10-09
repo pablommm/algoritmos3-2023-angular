28edfaafee465618f3a5f6e5dbb20a3c755d670a
francopolesel <francopolesel99@gmail.com>
2023-10-09 20:18:05 -0300

    Hago test de puntoDeVenta service

--- src/app/services/puntoDeVenta.service.spec.ts
+++ src/app/services/puntoDeVenta.service.spec.ts
@@ -1,16 +1,51 @@
 /* tslint:disable:no-unused-variable */
-import { PuntoDeVentaService } from './puntoDeVenta.service'
-import { TestBed, inject } from '@angular/core/testing'
-
+import { RouterTestingModule } from '@angular/router/testing'
+import { CardSobresComponent } from '../Componente/cardSobres/cardSobres.component'
+import { ActivatedRouteStub } from '../activatedRouteStub'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { PrecioSobrePipe } from 'src/app/pipes/precioSobre.pipe'
+import { PuntoDeVenta } from '../dominio/puntoDeVenta'
 
 describe('Service: PuntoDeVentaService', () => {
-  beforeEach(() => {
+  let fixture: ComponentFixture<CardSobresComponent>
+  let component: CardSobresComponent
+  const activatedRouteStub = new ActivatedRouteStub()
+
+  beforeEach(async () => {
     TestBed.configureTestingModule({
-      providers: [PuntoDeVentaService]
-    })
+      declarations: [CardSobresComponent, PrecioSobrePipe],
+      imports: [RouterTestingModule],
+      providers: [
+        { provide: activatedRouteStub, useValue: activatedRouteStub } // Proporciona ActivatedRouteStub como valor para ActivatedRoute.
+      ]
+    }).compileComponents()
   })
 
-  it('should ...', inject([PuntoDeVentaService], (service: PuntoDeVentaService) => {
-    expect(service).toBeTruthy()
-  }))
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CardSobresComponent)
+    component = fixture.componentInstance
+
+    const puntoDeVentaData: PuntoDeVenta = new PuntoDeVenta()
+    puntoDeVentaData.nombre = 'Carrefour'
+    puntoDeVentaData.direccion = 'Av. San Martin 4930'
+    puntoDeVentaData.ubicacionGeografica = '(34.9696; 38.5940)'
+    puntoDeVentaData.distancia = '3 kilómetros'
+    puntoDeVentaData.stock = 3
+    puntoDeVentaData.precio = '120'
+    puntoDeVentaData.tipo = 'Supermercado'
+
+    component.puntoDeVenta = puntoDeVentaData
+
+    fixture.detectChanges()
+  })
+
+  it('debería mostrar el icono de supermercado', () => {
+    const iconoSupermercado = fixture.nativeElement.querySelector(
+      '.card-upper-subcontainer i'
+    )
+
+    expect(iconoSupermercado.className).toContain(
+      'card-detalles-highlight fa-basket-shopping'
+    )
+  })
 })
