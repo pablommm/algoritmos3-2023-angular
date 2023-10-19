6b7fec5f972b224d6ad2af40fdba51320e966c27
pablommm <murgiapablo@gmail.com>
2023-10-19 00:07:39 -0300

    quitamos la logica de filtros en busquedaFigurita y busquedaFiguritaPerfil

--- src/app/page/busquedaFiguritas/busquedaFiguritas.component.ts
+++ src/app/page/busquedaFiguritas/busquedaFiguritas.component.ts
@@ -35,25 +35,6 @@ export class BusquedaFiguritasComponent implements OnInit {
       mostrarError(this, error)
     }
   }
-  filtros = new Filtros()
+  
 }
 
-export class Filtros {
-  desde = ''
-  hasta = ''
-  esPromesa = false
-  esOnFire = false
-
-  validacionHasta() {
-    if (this.desde != '' && this.hasta != '') {
-      if (this.desde > this.hasta) {
-        alert('El valor del campo desde no puede ser menor que el de hasta')
-      }
-    }
-  }
-
-  busqueda() {
-    this.validacionHasta()
-    alert(this.desde + '....' + this.hasta)
-  }
-}
--- src/app/page/busquedaFiguritasPerfil/busquedaFiguritasPerfil.component.ts
+++ src/app/page/busquedaFiguritasPerfil/busquedaFiguritasPerfil.component.ts
@@ -35,25 +35,7 @@ export class BusquedaFiguritasPerfilComponent implements OnInit {
       mostrarError(this, error)
     }
   }
-  filtros = new Filtros()
+
 }
 
-export class Filtros {
-  desde = ''
-  hasta = ''
-  esPromesa = false
-  esOnFire = false
 
-  validacionHasta() {
-    if (this.desde != '' && this.hasta != '') {
-      if (this.desde > this.hasta) {
-        alert('El valor del campo desde no puede ser menor que el de hasta')
-      }
-    }
-  }
-
-  busqueda() {
-    this.validacionHasta()
-    alert(this.desde + '....' + this.hasta)
-  }
-}
