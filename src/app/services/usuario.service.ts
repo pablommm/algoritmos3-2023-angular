1a06bd90045e7c6bd2781a5b1534502d1c6d63e9
pablommm <murgiapablo@gmail.com>
2023-10-29 18:29:41 -0300

    se inicia logica para traer tiposUsuarios

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -13,4 +13,28 @@ export class UsuarioService {
     /* const objetoUsuario = usuario[0] */
     return usuarios
   }
+
+  /*
+  async getTiposUsuarios() {
+    `${REST_SERVER_URL}/listaTiposUsuarios`
+  }
+*/
+
+  /*
+    async todasLasFiguritas(filtro: FiltroFiguritas) {
+    const figuritas$ = this.httpClient.get<FiguritaJSON[]>(
+      `${REST_SERVER_URL}/FiguritasRepetidas/0`,
+      { params: filtro.asHttpParams() }
+    )
+
+    const figuritaJSON = await lastValueFrom(figuritas$)
+    console.log(figuritaJSON)
+
+    return figuritaJSON.map((figuritaJSON: FiguritaJSON) =>
+      Figurita.fromJson(figuritaJSON)
+    )
+  }
+ */
+
+
 }
