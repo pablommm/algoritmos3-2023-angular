1b67e9867af6e11bf6bf2eb16657b9942cd1bb5f
pablommm <murgiapablo@gmail.com>
2023-11-05 01:03:33 -0300

    se arreglo test

--- src/app/services/Seleccion.service.spec.ts
+++ src/app/services/Seleccion.service.spec.ts
@@ -1,16 +1,16 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
-import { SeleccionService } from './Seleccion.service';
+import { TestBed, inject } from '@angular/core/testing'
+import { SeleccionService } from './Seleccion.service'
 
 describe('Service: Seleccion', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [SeleccionService]
-    });
-  });
+    })
+  })
 
   it('should ...', inject([SeleccionService], (service: SeleccionService) => {
-    expect(service).toBeTruthy();
-  }));
-});
+    expect(service).toBeTruthy()
+  }))
+})
