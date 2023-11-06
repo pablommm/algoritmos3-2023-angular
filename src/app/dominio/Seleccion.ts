3e475b738d73ca836297baf2c328953a719773ba
pablommm <murgiapablo@gmail.com>
2023-11-06 00:14:20 -0300

    cambios

--- src/app/dominio/Seleccion.ts
+++ src/app/dominio/Seleccion.ts
@@ -1,18 +1,18 @@
 export type SeleccionJSON = {
   pais: string
-  confederacion: string
-  copasDelMundo: number
-  copasConfederacion: number
-  id: number
+  //confederacion: string
+ // copasDelMundo: number
+  //copasConfederacion: number
+  //id: number
 }
 
 export class Seleccion {
   constructor(
     public pais: string = "",
-    public confederacion: string = "",
-    public copasDelMundo: number = 0,
-    public copasConfederacion: number = 0,
-    public id: number = 0
+   // public confederacion: string = "",
+    //public copasDelMundo: number = 0,
+    //public copasConfederacion: number = 0,
+    //public id: number = 0
   ) { }
   static fromJson(seleccion: object): Seleccion {
     console.log("pase por el dominio selecion")
